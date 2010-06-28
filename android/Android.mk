a16797b9e8968b20d4d0fcc64af86e26dfbf78a4
Andrew Kulikov <a.kulikov@f5.com>
2010-06-28 15:30:21 -0700

    Support split tunneling for VPN connection in pppd. Split tunneling information is passed to ip-up-vpn and ip-down-vpn in a 'ipparam' option in the format <routelist>;<exclude_route_list>. Where <routelist> is a comma-separated list of subnets to be routable through PPP interface, <exclude_route_list> is a comma-separated list of subnets to be routed through default gateway. If ipparam is not set, then all traffic is redirected to PPP interface (default behaviour).
    Change-Id: I40e656f063cb6e37ac2887a171fd064d61317874

--- android/Android.mk
+++ android/Android.mk
@@ -17,9 +17,16 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES := ip-up-vpn.c
+LOCAL_SRC_FILES := vpn-routes.c ip-up-vpn.c
 LOCAL_SHARED_LIBRARIES := libcutils
 LOCAL_MODULE := ip-up-vpn
 LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/ppp
 
+include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := vpn-routes.c ip-down-vpn.c
+LOCAL_SHARED_LIBRARIES := libcutils
+LOCAL_MODULE := ip-down-vpn
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/ppp
 include $(BUILD_EXECUTABLE)
